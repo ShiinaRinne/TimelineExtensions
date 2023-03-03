38f182872f452f07ac71044c1c961bf362446249
ShiinaRinne <ym@youngmoe.com>
2023-03-03 11:53:58 +0800

    add missing scripts

---
+++ Editor/Extensions.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Linq;
+using UnityEngine.Rendering;
+
+namespace YMToonURP.Scripts
+{
+    public static class YMExtensions
+    {
+        /// Return a titlecased version of the string
+        /// Examples: "elysia".Title() => "Elysia"
+        public static string Title(this string str)
+        {
+            return str.First().ToString().ToUpper() + str.Substring(1);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vp"></param>
+        public static T GetValue<T>(this VolumeParameter vp)
+        {
+            return vp.GetValue<T>();
+        }
+
+    }
+}
